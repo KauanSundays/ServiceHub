bfa3b07af0d8be10d02cd935a7ead97220cbaf31
Kauan Rocha <ricardo.kauand@gmail.com>
2026-01-12 10:08:29 -0300

    feat: :control_knobs: registrar rotas  para o TicketController.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -20,6 +21,7 @@ Route::get('dashboard', function () {
 Route::middleware(['auth'])->group(function () {
     Route::resource('companies', CompanyController::class);
     Route::resource('projects', ProjectController::class);
+    Route::resource('tickets', TicketController::class);
 });
 
 require __DIR__.'/settings.php';
